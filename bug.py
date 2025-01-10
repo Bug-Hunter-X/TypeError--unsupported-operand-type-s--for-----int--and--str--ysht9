39fc9c507d9899ad7473fafd708f5e2c264674ac
BugHunter-X <askzenai@gmail.com>
2025-01-10 09:46:06 +0100

    Add bug.py file

---
+++ bug.py
@@ -0,0 +1,18 @@
+def calculate_average(numbers):
+    if not numbers:
+        return 0  # Handle empty list case
+    total = sum(numbers)
+    average = total / len(numbers)
+    return average
+
+my_list = []
+result = calculate_average(my_list)
+print(f"The average is: {result}") #This will print 0 as intended
+
+my_list = [10,20,30,40,50]
+result = calculate_average(my_list)
+print(f"The average is: {result}") #This will print 30.0 as intended
+
+my_list = [10,20,30,40,50,'a']
+result = calculate_average(my_list) #This will throw TypeError: unsupported operand type(s) for +: 'int' and 'str'
+print(f"The average is: {result}")
